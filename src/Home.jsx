a2a5d4c85787b69d519da3ec6a5196fafe88e0a0
sahilkandpal <sahilkandpal2003@gmail.com>
2022-10-08 11:09:19 +0530

    add feed route

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+
 function Home(){
     return <>
         <div>This is the home page.</div>
+        <Link to="/order-food-online">order food</Link>
     </>
 }
 

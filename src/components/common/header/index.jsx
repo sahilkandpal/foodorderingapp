3897635b02baa2b35754210f78dd8ddf9b731373
Shubham <108388327+kumarShubham23@users.noreply.github.com>
2022-10-12 21:33:45 +0530

    initial phase of creating navbar

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import "./header.css";
 const Header = () => {
   return (
-    <div>
-      This is header
+    <div className='max-width header'>
+      <img src="Logo.png" alt="Yippy-logo" className='header-logo' />
+      <div className='header-right'>
+        <div className="header-location">
+          <div className="location-wrapper">
+            <div className="location-icon">
+            <span className="material-symbols-outlined absolute-center">location_on</span>
+            <div>Delhi</div>
+            </div>
+            <span class="material-symbols-outlined absolute-center">arrow_drop_down</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
